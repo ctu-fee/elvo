9e34bec4074feaf21212506b76f931616c058fd0
Ivan Novakov <ivan.novakov@debug.cz>
2013-11-28 10:05:21 +0100

    candidate result class

---
+++ module/Elvo/src/Elvo/Domain/Vote/Processor/CandidateResult.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Elvo\Domain\Vote\Processor;
+
+use Elvo\Domain\Entity\Candidate;
+
+
+/**
+ * Holds the vote count for a single candidate.
+ */
+class CandidateResult
+{
+
+    /**
+     * @var Candidate
+     */
+    protected $candidate;
+
+    /**
+     * @var integer
+     */
+    protected $numVotes;
+
+
+    /**
+     * Constructor.
+     * 
+     * @param Candidate $candidate
+     * @param intefer $numVotes
+     */
+    public function __construct(Candidate $candidate, $numVotes = 0)
+    {
+        $this->candidate = $candidate;
+        $this->numVotes = $numVotes;
+    }
+
+
+    /**
+     * @return Candidate
+     */
+    public function getCandidate()
+    {
+        return $this->candidate;
+    }
+
+
+    /**
+     * @return integer
+     */
+    public function getNumVotes()
+    {
+        return $this->numVotes;
+    }
+
+
+    /**
+     * Adds provided amount to the vote count.
+     * 
+     * @param integer $numVotes
+     */
+    public function addVotes($numVotes)
+    {
+        $this->numVotes += $numVotes;
+    }
+
+
+    /**
+     * Adds one vote to the vote count.
+     */
+    public function addOne()
+    {
+        $this->addVotes(1);
+    }
+}
---
+++ module/Elvo/tests/unit/ElvoTest/Domain/Vote/Processor/CandidateResultTest.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace ElvoTest\Domain\Vote\Processor;
+
+use Elvo\Domain\Vote\Processor\CandidateResult;
+
+
+class CandidateResultTest extends \PHPUnit_Framework_TestCase
+{
+
+
+    public function testConstructorWithImplicitNumVotes()
+    {
+        $candidate = $this->getCandidateMock();
+        $cr = new CandidateResult($candidate);
+        
+        $this->assertSame(0, $cr->getNumVotes());
+        $this->assertSame($candidate, $cr->getCandidate());
+    }
+
+
+    public function testConstructorWithExplicitNumVotes()
+    {
+        $candidate = $this->getCandidateMock();
+        $num = 108;
+        $cr = new CandidateResult($candidate, $num);
+        
+        $this->assertSame($num, $cr->getNumVotes());
+        $this->assertSame($candidate, $cr->getCandidate());
+    }
+    
+    /*
+     * 
+     */
+    protected function getCandidateMock()
+    {
+        $candidate = $this->getMock('Elvo\Domain\Entity\Candidate');
+        return $candidate;
+    }
+}
