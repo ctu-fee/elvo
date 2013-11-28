d5ad11fdb94e74ca06d4f4a960b2fbd65f0a3e30
Ivan Novakov <ivan.novakov@debug.cz>
2013-11-28 11:12:03 +0100

    added funcionality co CandidateResultCollection

--- module/Elvo/src/Elvo/Domain/Entity/Collection/CandidateResultCollection.php
+++ module/Elvo/src/Elvo/Domain/Entity/Collection/CandidateResultCollection.php
@@ -3,6 +3,7 @@
 namespace Elvo\Domain\Entity\Collection;
 
 use Elvo\Domain\Entity\CandidateResult;
+use Elvo\Domain\Entity\Candidate;
 
 
 /**
@@ -10,6 +11,65 @@ use Elvo\Domain\Entity\CandidateResult;
  */
 class CandidateResultCollection extends AbstractCollection
 {
+
+
+    /**
+     * Add one vote to the result of the corresponding candidate.
+     * 
+     * @param Candidate $candidate
+     */
+    public function addVoteToCandidate(Candidate $candidate)
+    {
+        $candidateResult = $this->findByCandidate($candidate);
+        if (! $candidateResult) {
+            $candidateResult = $this->createCandidateResult($candidate);
+            $this->append($candidateResult);
+        }
+        
+        $candidateResult->addOne();
+    }
+
+
+    /**
+     * Finds the result for the corresponding candidate.
+     * 
+     * @param Candidate $candidate
+     * @return CandidateResult|null
+     */
+    public function findByCandidate(Candidate $candidate)
+    {
+        return $this->findByCandidateId($candidate->getId());
+    }
+
+
+    /**
+     * Finds the result for the candidate with the provided ID.
+     * 
+     * @param integer $candidateId
+     * @return CandidateResult|null
+     */
+    public function findByCandidateId($candidateId)
+    {
+        foreach ($this->items as $candidateResult) {
+            if ($candidateId == $candidateResult->getCandidate()->getId()) {
+                return $candidateResult;
+            }
+        }
+        
+        return null;
+    }
+
+
+    /**
+     * Creates a candidate result for the provided candidate.
+     * 
+     * @param Candidate $candidate
+     * @return CandidateResult
+     */
+    protected function createCandidateResult(Candidate $candidate)
+    {
+        return new CandidateResult($candidate);
+    }
 
 
     /**
